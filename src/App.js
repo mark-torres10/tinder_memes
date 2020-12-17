201e4450b4262f95daf057a3b9ec273442294035
mark-torres-10 <markptorres1@gmail.com>
2020-12-17 10:46:09 -0500

    add buttons to bottom of card

--- src/App.js
+++ src/App.js
@@ -10,9 +10,11 @@ function App() {
         {/* Add spacing between cards? */}
         <Card url="https://tse2.mm.bing.net/th?id=OIP.7b2nem-RbKmraEUU8SOH4AAAAA&pid=Api"/>
         
+        {/*
         <Card url="https://qph.fs.quoracdn.net/main-qimg-6d32bddfb0b0e98a46bb87d8cfafa8e1-c"/>
         
         <Card url="https://qph.fs.quoracdn.net/main-qimg-e8615c16d14b07c8210591ceb5317fc0"/>
+        */}
         
       </header>
     </div>
--- src/Card.js
+++ src/Card.js
@@ -11,24 +11,33 @@ const Card = ({url}) => {
             <head>
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
             </head>
-            <h1>Here's a card!</h1>
+            <div className="top-of-card">
+                <h1>Here's a card!</h1>
+            </div>
             {/* Meme image */} 
             <img className="meme-image" src={url} alt=""/>
             {/* Add buttons on the bottom (via Font Awesome) */}
             <div className="bottom-of-card">
                 {/* return button */}
-                <span className="reaction-icons">
-                    <i class="fa fa-circle fa-stack-2x"></i>
+                <div className="reaction-icons">
                     <i class="fa fa-undo fa-stack-1x fa-inverse"></i>
-                </span>
+                </div>
                 {/* X button */}
-                <span className="reaction-icons">
-                    <i class="fa fa-circle fa-stack-2x"></i>
-                    <i class="fa fa-undo fa-stack-1x fa-inverse"></i>
-                </span>
+                <div className="reaction-icons">
+                    <i class="fa fa-times fa-stack-1x fa-inverse"></i>
+                </div>
                 {/* Star button */ }
+                <div className="reaction-icons">
+                    <i class="fa fa-star fa-stack-1x fa-inverse"></i>
+                </div>
                 {/* Heart button */}
+                <div className="reaction-icons">
+                    <i class="fa fa-heart fa-stack-1x fa-inverse"></i>
+                </div>
                 {/* Lightning button */}
+                <div className="reaction-icons">
+                    <i class="fa fa-bolt fa-stack-1x fa-inverse"></i>
+                </div>
             </div>
             
 
