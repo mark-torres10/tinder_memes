fa5cb947357b1d561bb1e40068dace789063719a
mark-torres-10 <markptorres1@gmail.com>
2020-12-17 02:11:03 -0500

    start adding icons

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react'; 
 import './App.css';
 import Card from './Card'; 
 
@@ -5,8 +6,14 @@ function App() {
   return (
     <div className="App">
       <header className="homepage">
-        <h1>Start of Tinder Memes app!</h1>
-        <Card />
+        <h1>Start of Tinder Memes app!</h1>; 
+        {/* Add spacing between cards? */}
+        <Card url="https://tse2.mm.bing.net/th?id=OIP.7b2nem-RbKmraEUU8SOH4AAAAA&pid=Api"/>
+        
+        <Card url="https://qph.fs.quoracdn.net/main-qimg-6d32bddfb0b0e98a46bb87d8cfafa8e1-c"/>
+        
+        <Card url="https://qph.fs.quoracdn.net/main-qimg-e8615c16d14b07c8210591ceb5317fc0"/>
+        
       </header>
     </div>
   );
--- src/Card.js
+++ src/Card.js
@@ -1,20 +1,38 @@
 import React from 'react'; 
 import './Card.css'; 
+import 'react-icons/fa'; 
 
-const Card = (props) => {
 
-    {/* Create arr of image links to loop through */}
-    const test_links  = ["https://tse2.mm.bing.net/th?id=OIP.7b2nem-RbKmraEUU8SOH4AAAAA&pid=Api", 
-                         "https://qph.fs.quoracdn.net/main-qimg-6d32bddfb0b0e98a46bb87d8cfafa8e1-c", 
-                         "https://qph.fs.quoracdn.net/main-qimg-e8615c16d14b07c8210591ceb5317fc0"]; 
 
+const Card = ({url}) => {
 
      return (
         <div className="card">
+            <head>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+            </head>
             <h1>Here's a card!</h1>
-            {/* Loop through all images */} 
-            <img className="meme_image" src={test_links[0]} alt="">
-            </img>
+            {/* Meme image */} 
+            <img className="meme-image" src={url} alt=""/>
+            {/* Add buttons on the bottom (via Font Awesome) */}
+            <div className="bottom-of-card">
+                {/* return button */}
+                <span className="reaction-icons">
+                    <i class="fa fa-circle fa-stack-2x"></i>
+                    <i class="fa fa-undo fa-stack-1x fa-inverse"></i>
+                </span>
+                {/* X button */}
+                <span className="reaction-icons">
+                    <i class="fa fa-circle fa-stack-2x"></i>
+                    <i class="fa fa-undo fa-stack-1x fa-inverse"></i>
+                </span>
+                {/* Star button */ }
+                {/* Heart button */}
+                {/* Lightning button */}
+            </div>
+            
+
+
         </div>
     )
 } 
